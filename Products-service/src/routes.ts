563f7254a665c6d5c64c5e3a4aafccf5694ccc84
Goran Jakovljević <goranjako@gmail.com>
2022-07-20 23:46:08 +0200

    test

--- Products-service/src/routes.ts
+++ Products-service/src/routes.ts
@@ -8,23 +8,23 @@ const {
 import ProductController from "./controllers/product.controller";
 import OrderController from "./controllers/order.controller";
 
-//import authManager from './util/auth';
+import authManager from './util/auth';
 export default function setRoutes(app: any) {
   const router = express.Router();
 
   //productRoute
   router
     .route("/product")
-    .post(validateProductBody(), validate, ProductController.create);
-  router.route("/product").get(ProductController.getAll);
-  router.route("/product/:id").get(ProductController.get);
+    .post(authManager.verifyToken,validateProductBody(), validate, ProductController.create);
+  router.route("/product").get(authManager.verifyToken,ProductController.getAll);
+  router.route("/product/:id").get(authManager.verifyToken,ProductController.get);
   router
     .route("/product/:id")
-    .put(validateProductBody(), validate, ProductController.put);
-  router.route("/product/:id").delete(ProductController.delete);
+    .put(authManager.verifyToken,validateProductBody(), validate, ProductController.put);
+  router.route("/product/:id").delete(authManager.verifyToken,ProductController.delete);
   router
     .route("/orders")
-    .post(validateOrderBody(), validate, OrderController.create);
+    .post(authManager.verifyToken,validateOrderBody(), validate, OrderController.create);
 
   app.use("/", router);
 }
